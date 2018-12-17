af6170dd5bba163fe3fbbb16352ab8fe8b7b51d0
Michael Lacanilao <mlacanil@montgomerycollege.edu>
2018-12-16 22:04:06 -0500

    Fix spacing

--- Homework #2 - Student Information/HW2.cpp
+++ Homework #2 - Student Information/HW2.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main ()
 {
 	cout << "My name is Michael Lacanilao.\n"
-		<< "My address is XXXX.\n"
-		<< "My telephone number is (XXX) XXX-XXXX.\n"
-		<< "My college major is Computer Science.\n";
+	     << "My address is XXXX.\n"
+	     << "My telephone number is (XXX) XXX-XXXX.\n"
+	     << "My college major is Computer Science.\n";
 	system("pause");
 	return 0;
 }
