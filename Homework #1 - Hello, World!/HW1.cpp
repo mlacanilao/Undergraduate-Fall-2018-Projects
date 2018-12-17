6722db57c56a0b5fa4033cb0cb63e2c1566da44b
Michael Lacanilao <mlacanil@montgomerycollege.edu>
2018-12-16 22:03:04 -0500

    Fix spacing

--- Homework #1 - Hello, World!/HW1.cpp
+++ Homework #1 - Hello, World!/HW1.cpp
@@ -1,14 +1,12 @@
 // Michael Lacanilao. This is my homework.
 
-#include <iostream>               // needed to perform C++ I/O
+#include <iostream> // needed to perform C++ I/O
 using namespace std;
 
 int main ()
 
 {
-
 	cout << "Hello, World!" << endl;
 	system("pause");
 	return 0;
-
 }
