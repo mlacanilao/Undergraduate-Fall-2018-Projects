cbcce215842116c874c3a2336a84ed8f22916fa6
Michael John Lacanilao <mlacanil@montgomerycollege.edu>
2019-08-15 15:10:00 -0400

    Add updated Homework #1

---
+++ Homework1 - Hello, World!/Homework1 - Hello, World!/Source.cpp
@@ -0,0 +1,20 @@
+// Michael Lacanilao
+
+// Needed to perform C++ I/O
+#include <iostream>
+
+// Declares that the program will be accessing entities whose names are part of the space called std
+using namespace std;
+
+int main()
+{
+	// Prints "Hello, World!" to the screen
+	cout << "Hello, World!" << endl;
+
+	// Pauses screen
+	system("pause");
+
+	// Sends the integer value 0 back to the operating system upon the program's completion.
+	// The value 0 usually indicates that a program executed sucessfully.
+	return 0;
+}
